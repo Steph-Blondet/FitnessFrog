8def4aaca62cdfee96fae81c1346931af5f86bdd
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-01-17 06:05:27 -0500

    Adding positive feedback when the user enters their exercise time

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -15,6 +15,7 @@ namespace Treehouse.Fitness.Frog
                 Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
                 string entry = Console.ReadLine();
 
+
                 if(entry == "quit")
                 {
                     keepGoing = false;
@@ -22,6 +23,22 @@ namespace Treehouse.Fitness.Frog
                 else
                 {
                     int minutes = int.Parse(entry);
+
+                    if(minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if(minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if(minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                    }
 
                     // Add minutes exercised to total
                     runningTotal = runningTotal + minutes;
