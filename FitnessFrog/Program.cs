6a0397ee2dccebd6c6d7b70bd374be9f9032ee46
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-01-16 20:07:47 -0500

    Add minutes exercised to total

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -1,20 +1,25 @@
 using System; //declaring it here so we can stop writing "System" everytime.
 
 namespace Treehouse.Fitness.Frog
-
 {
     class Program
     {
         static void Main()
         {
+            int runningTotal = 0;
+
             // Prompt the user for minutes exercised
             Console.Write("Enter how many minutes you exercised: ");
 
             string entry = Console.ReadLine();
 
+            int minutes = int.Parse(entry);
+
             // Add minutes exercised to total
+            runningTotal = runningTotal + minutes;
+
             // Display total minutes exercised to the screen
-            Console.WriteLine("You've entered " + entry + " minutes");
+            Console.WriteLine("You've entered " + runningTotal + " minutes");
             // Repeat until the user quits
         }
     }
